5cc649764bfffd0ee98d4133b0409825d28a0fbf
Michael <michael.nilga@gmail.com>
2024-12-03 17:18:38 +0200

    Classes added according to interfaces

---
+++ DevOps_Basics/DevOps_Basics/DevOps_Basics/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps_Basics
+{
+    public class Book : IBook
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string title, string author, int year)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+        }
+
+        public override string ToString()
+        {
+            return $"\"{Title}\" by {Author}, {Year}";
+        }
+    }
+}
